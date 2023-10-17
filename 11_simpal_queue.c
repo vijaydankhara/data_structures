04732c902ec864e4eeccd783b51e13e02e9c19c7
vijay dankhara <vijaydankhara111@gmail.com>
2023-10-17 08:08:59 +0530

    17-10-2023

---
+++ 11_simpal_queue.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#define n 5
+int a[n] , f =-1 , r =-1;
+
+int first_Insert(int data)
+{
+    if(r >= n-1)
+    {
+        printf("Queue Is Full...");
+    }
+    else if(f < 0)
+    {
+        f = r = 0;
+        a[r] = data;
+    }
+    else
+    {
+        r++;
+        a[r] = data;
+    }
+}
+
+int first_Delete()
+{
+    if (f == r)
+    {
+        printf("Queue Is Empty...");
+        return -1;
+    }
+    else if (f < 0)
+    {
+        f = r = -1;
+    }
+    else
+    {
+        f++;
+    }
+}
+
+int display()
+{
+    if (f<0)
+    {
+        printf("queue is empty...");
+    }
+    
+    for (int i = f; i <= r; i++)
+    {
+        printf("%d\t",a[i]);
+    }   
+}
+
+int main()
+{
+    first_Insert(10);
+    first_Insert(20);
+    first_Insert(30);
+    first_Insert(40);
+    first_Insert(50);
+    first_Delete(40);
+    display();
+}
